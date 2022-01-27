f0a143d6eab0530f1987b71065ac3824eff1083d
ZiyunClaudeWang <ziyunw@seas.upenn.edu>
2022-01-27 02:43:41 +0000

    cvpr batch

--- python/batch_recons.bash
+++ python/batch_recons.bash
@@ -4,5 +4,5 @@ search_dir=$1
 echo "$search_dir"
 for entry in "$search_dir"/*.h5;
 do
-    python offline_reconstruction.py  --freq_hz 60 --upsample_rate 1 --h5file $entry --height 480 --width 640
+    python offline_reconstruction.py  --freq_hz 60 --upsample_rate 1 --h5file $entry --height 480 --width 640 --gpu_id 1
 done
